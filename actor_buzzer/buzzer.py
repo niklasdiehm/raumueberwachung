b443cddefebf27bfdbda787cbf6c39688d759037
Niklas Diehm <diehm.niklas@student.dhbw-karlsruhe.de>
2022-06-13 22:59:14 +0200

    Actor buzzer bind to humidity

--- actor_buzzer/buzzer.py
+++ actor_buzzer/buzzer.py
@@ -1,24 +1,62 @@
 import RPi.GPIO as GPIO
 import time
+import os
+import requests
+from datetime import datetime, timedelta
+from datetime import timezone
+
+actors_on = os.environ.get("ACTORS_ON")
+if actors_on != "1":
+    exit()
 
 GPIO.setmode(GPIO.BCM)
-
-# Hier wird der Eingangs-Pin deklariert, an dem der Sensor angeschlossen ist.
+# Hier wird der Ausgangs-Pin deklariert, an dem der Buzzer angeschlossen ist.
 Buzzer_PIN = 24
 GPIO.setup(Buzzer_PIN, GPIO.OUT, initial=GPIO.LOW)
 
-print("Buzzer-Test [druecken Sie STRG+C, um den Test zu beenden]")
+middleware_url = os.environ.get("MIDDLEWARE_URL")
+middleware_api_key = os.environ.get("MIDDLEWARE_API_KEY")
+device_id = os.environ.get("RESIN_DEVICE_UUID")
+room_name = os.environ.get("ROOM_NAME")
+measurement_interval_secs = int(os.environ.get("MEASUREMENT_INTERVAL_SECS"))
+actors_humidity_threshold_percent = int(
+    os.environ.get("ACTORS_HUMIDITY_THRESHOLD_PERCENT")
+)
 
 # Hauptprogrammschleife
 try:
     while True:
-        print("Buzzer 1 Sekunden an")
-        GPIO.output(Buzzer_PIN, GPIO.HIGH)  # Buzzer wird eingeschaltet
-        time.sleep(1)  # Wartemodus für 1 Sekunde
-        print("Buzzer 15 Sekunden aus")
-        GPIO.output(Buzzer_PIN, GPIO.LOW)  # Buzzer wird ausgeschaltet
-        time.sleep(15)
+        time_to_middleware = datetime.now(timezone.utc) - timedelta(
+            seconds=measurement_interval_secs * 3
+        )
+        request = requests.get(middleware_url +
+                               "?code=" + middleware_api_key +
+                               "&deviceID=" + device_id +
+                               "&timestamp[after]=" +
+                               time_to_middleware.strftime(
+                                    "%Y-%m-%d %H:%M:%S"
+                                ) +
+                               "&measurementName=currentHumidity")
+        data = request.json()
+        # Hier wird die aktuelle Luftfeuchtigkeit ermittelt
+        newlist = sorted(data, key=lambda d: d['timestamp'])
+        if len(newlist) > 0:
+            current_humidity = newlist[-1]['measurementValue']
+        else:
+            current_humidity = 0
+        print("Current humidity: " + str(current_humidity))
+        print("Humidity threshold: " + str(actors_humidity_threshold_percent))
+        # Wenn die Luftfeuchtigkeit oberhalb des Schwellwertes ist,
+        # wird der Buzzer für eine Sekunde eingeschaltet und dann wieder ein
+        # Intervall abgewartet. Ansonsten wird das Intervall abgewartet.
+        if current_humidity > actors_humidity_threshold_percent:
+            GPIO.output(Buzzer_PIN, GPIO.HIGH)
+            time.sleep(1)
+            GPIO.output(Buzzer_PIN, GPIO.LOW)
+        time.sleep(measurement_interval_secs)
 
 # Aufraeumarbeiten nachdem das Programm beendet wurde
 except KeyboardInterrupt:
+    GPIO.cleanup()
+finally:
     GPIO.cleanup()
