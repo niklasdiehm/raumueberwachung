4dc15fb8e9bbe7cde1706e4ad66ac83672259957
Niklas Diehm <diehm.niklas@student.dhbw-karlsruhe.de>
2022-06-21 09:34:48 +0200

    Add predictions tests

---
+++ predictions/get_temperature_data.py
@@ -0,0 +1,28 @@
+import requests
+from datetime import datetime, timedelta
+from datetime import timezone
+import pandas as pd
+
+# Get the data from the Azure IoT Hub
+# We want to get the data from the last 7 days
+# we iterate over the hours and get the data for each hour
+# after that, we append the data to a list and output a csv file
+
+current_date = datetime.now(timezone.utc).replace(tzinfo=timezone.utc)
+date_seven_days_before = current_date - timedelta(days=7)
+
+results = []
+while (date_seven_days_before < current_date):
+    date_seven_days_before = date_seven_days_before + timedelta(hours=1)
+    request = requests.get("https://fa-roomitor.azurewebsites.net/api/measurements?code=VOXG7U915GhnWo1dRCd-wuNrhgM897uDp4Lae4-kTKZCAzFu7O0DIA==" +
+                           "&timestamp[after]=" +
+                           date_seven_days_before.strftime("%Y-%m-%d %H:%M:%S") +
+                           "&timestamp[before]=" +
+                            (date_seven_days_before + timedelta(hours=1)).strftime("%Y-%m-%d %H:%M:%S") +
+                           "&deviceID=2187733e6049fabe904a1a730fe7c457" + 
+                           "&measurementName=currentTemperature")
+    results.extend(request.json())
+df = pd.DataFrame.from_records(results)
+df.drop(columns=["_self", "_etag", "_attachments", "_ts", "id", "_rid"], inplace=True)
+print(df.head())
+df.to_csv("temperature_data.csv", index=False)
